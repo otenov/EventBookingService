8931d35769d0d6eb9553c8860bce8c37668c2eff
Сергей Отенов <serg10.otenov@yandex.ru>
2026-04-11 03:48:10 +0300

    refactor: исправлена модель данных event. добавлен контроллер и реализованы crud.

---
+++ Controllers/EventController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventBookingService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+    }
+}
