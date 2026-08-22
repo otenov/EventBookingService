e243a3d0777ca1325d4e7916834d8f7c11702772
Sergey Otenov <serg10.otenov@yandex.ru>
2026-08-22 13:13:43 +0300

    chore: add swagger

--- Program.cs
+++ Program.cs
@@ -7,7 +7,13 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IEventService, EventService>();
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
