db62b2fddf8440a2697c318a5137b6d5fab4fd61
Sergey Otenov <serg10.otenov@yandex.ru>
2026-08-22 12:33:10 +0300

    chore: register services in DI

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using EventBookingService;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
